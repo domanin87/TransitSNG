e0b142ebab54db3db2ee1eff69c2fe99ef2499bf
domanin87 <domanin87@gmail.com>
2025-09-15 00:17:17 +0500

    3.0.12
    run_migration

--- backend/migrations/run_migrations.js
+++ backend/migrations/run_migrations.js
@@ -1,7 +1,5 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
-dotenv.config();
+const { Sequelize } = require("sequelize");
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
