dc39221c8d117793ecea19e4cefd72d7d3b46713
domanin87 <domanin87@gmail.com>
2025-09-21 14:53:10 +0500

    Update sequelize.js

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -2,12 +2,22 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || "mydb",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
